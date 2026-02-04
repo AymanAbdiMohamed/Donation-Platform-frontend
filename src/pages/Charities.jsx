b226e6f00923db519de61b4d348e5f4cf47ab26b
Michelle Nyagaka <michellenyagaka@gmail.com>
2026-02-04 20:47:27 +0300

    ensure charities are fetched and approved

--- src/pages/Charities.jsx
+++ src/pages/Charities.jsx
@@ -1,2 +1,12 @@
 import { useEffect, useState } from "react";
 import CharityCard from "../components/CharityCard";
+
+function Charities() {
+  const [charities, setCharities] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/charities?status=approved")
+      .then((res) => res.json())
+      .then((data) => setCharities(data))
+      .catch((error) => console.error(error));
+  }, []);}
