59156396a7f75c496223d1622bb73ea1c51640b4
Michelle Nyagaka <michellenyagaka@gmail.com>
2026-02-06 14:37:48 +0300

    Fix nested BrowserRouter and API calls

--- src/pages/Charities.jsx
+++ src/pages/Charities.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import CharityCard from "../components/CharityCard";
+import { api } from "../api";
 
 function Charities() {
   const [charities, setCharities] = useState([]);
 
   useEffect(() => {
-    fetch("/api/charities?status=approved")
-      .then((res) => res.json())
-      .then((data) => setCharities(data))
+    api.get("/charity?status=approved")
+      .then((res) => setCharities(res.data))
       .catch((error) => console.error(error));
   }, []);
 
